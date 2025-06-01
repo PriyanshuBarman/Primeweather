d4dd10ca9cb7988fbc67cdde547d78454246601c
Priyanshu Barman <priyanshubarman741@gmail.com>
2025-06-01 11:33:24 +0530

    add vercel.json file

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
 import ReactGA from "react-ga4";
 
-ReactGA.initialize("G-GWJB5P73CQ");
+const GA_MEAS_ID = import.meta.env.VITE_GA_MEASUREMENT_ID;
+
+ReactGA.initialize(GA_MEAS_ID);
 
 ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
